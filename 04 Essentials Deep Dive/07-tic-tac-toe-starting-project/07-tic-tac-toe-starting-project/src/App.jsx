916ce64b4ddfee6c59474a1b6d3dac9a058351eb
ayan878 <ayanraza197@gmail.com>
2024-02-17 00:42:43 +0530

    tic-tac toe UI

--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,27 @@
-
 function App() {
-  
-
   return (
-   <header>
-    <img src="" alt=""/>
-    <h1>Tic-Tac-Toe</h1>
-   </header>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-name">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name"></span>Player 2
+              <span className="player-name">0</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        <div className="game-board"></div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;

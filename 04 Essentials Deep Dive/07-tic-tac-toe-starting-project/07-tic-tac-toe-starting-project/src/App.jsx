6cc4e8b21f21e90e5c9b006597115f9881b87999
ayan878 <ayanraza197@gmail.com>
2024-02-17 01:58:30 +0530

    game board UI

--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,14 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>
-          <Player initialName="Player 2" symbol="0"/>
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="0" />
         </ol>
-        <div className="game-board"></div>
+        <GameBoard />
       </div>
     </main>
   );

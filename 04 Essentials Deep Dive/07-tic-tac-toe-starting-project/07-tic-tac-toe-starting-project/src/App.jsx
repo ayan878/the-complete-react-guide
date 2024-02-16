d41b26b73aa88ba4727463fe2f48bd86572497af
ayan878 <ayanraza197@gmail.com>
2024-02-17 01:38:47 +0530

    Player Functioanality

--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,22 +1,11 @@
+import Player from "./components/Player";
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-name">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name"></span>Player 2
-              <span className="player-name">0</span>
-            </span>
-            <button>Edit</button>
-          </li>
+          <Player initialName="Player 1" symbol="X"/>
+          <Player initialName="Player 2" symbol="0"/>
         </ol>
         <div className="game-board"></div>
       </div>
--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,30 @@
-function Player({ name, symbol }) {
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+  const handleChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-name">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }

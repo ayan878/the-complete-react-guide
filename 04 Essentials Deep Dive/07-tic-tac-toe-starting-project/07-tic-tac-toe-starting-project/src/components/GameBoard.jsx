3d84852c1c0e3182075a8b46bcca43ff30c431e9
ayan878 <ayanraza197@gmail.com>
2024-02-17 13:02:46 +0530

    game board Functionality

--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 const initialGameBoard = [
   [null, null, null],
@@ -6,15 +6,31 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
@@ -23,5 +39,3 @@ function GameBoard() {
     </ol>
   );
 }
-
-export default GameBoard;
--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
-function Player({ initialName, symbol }) {
-  const [isEditing, setIsEditing] = useState(false);
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const handleEditClick = () => {
+  function handleEditClick() {
     setIsEditing((editing) => !editing);
-  };
-  const handleChange = (e) => {
-    setPlayerName(e.target.value);
-  };
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   let editablePlayerName = <span className="player-name">{playerName}</span>;
+
 
   if (isEditing) {
     editablePlayerName = (
@@ -19,14 +22,12 @@ function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
-        <span className="player-name">{symbol}</span>
+        <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
-
-export default Player;

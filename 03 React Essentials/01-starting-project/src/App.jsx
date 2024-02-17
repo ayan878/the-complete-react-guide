396f774dd1dc47cd3b340f0f83b5335f61960d14
ayan878 <ayanraza197@gmail.com>
2024-02-18 01:23:12 +0530

    App Components

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,22 +1,6 @@
 import React from "react";
-import {coreConcepts} from "./data.js";
-
+import { CoreConcept } from "./CoreConcept.jsx";
 import { Header } from "./Header.jsx";
-
-
-function CoreConcept() {
-  return (
-    <>
-      {coreConcepts.map((concept, index) => (
-        <li key={index}>
-          <img src={concept.img} alt={concept.title} />
-          <h3>{concept.title}</h3>
-          <p>{concept.description}</p>
-        </li>
-      ))}
-    </>
-  );
-}
 
 function App() {
   return (

3b9656e9fefa73bbc066746d09ea230de84f2214
ayan878 <ayanraza197@gmail.com>
2024-02-18 01:17:23 +0530

    reusable data

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,35 +1,8 @@
 import React from "react";
+import {coreConcepts} from "./data.js";
 import reactImg from "./assets/react-core-concepts.png";
-import jsxImg from "./assets/jsx-ui.png";
-import configImg from "./assets/config.png";
-import stateMgmtImg from "./assets/state-mgmt.png";
 
-const coreConcepts = [
-  {
-    title: "Components",
-    description:
-      "The core building UI block - compose the user interface by combining multiple components",
-    img: reactImg,
-  },
-  {
-    title: "JSX",
-    description:
-      "Return the (potentially dynamic) HTML code to define the actual markup that will be rendered",
-    img: jsxImg,
-  },
-  {
-    title: "Props",
-    description:
-      "Make Components configurable (and therefore reusable) by passing input data to them",
-    img: configImg,
-  },
-  {
-    title: "State",
-    description:
-      "React-managed data which, when changed, causes the components to re-render & the UI to update",
-    img: stateMgmtImg,
-  },
-];
+
 
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 

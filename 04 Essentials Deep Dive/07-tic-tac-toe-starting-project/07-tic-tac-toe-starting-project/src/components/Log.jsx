9a7e81fed62c3b17639b97c21998526df4de0802
ayan878 <ayanraza197@gmail.com>
2024-02-19 17:43:57 +0530

    finished

--- 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 04 Essentials Deep Dive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-
-function Log({ turns }) {
+export default function Log({ turns }) {
   return (
     <ol id="log">
-      {turns.map((turn, index) => (
-        <li key={index}>
-          {turn.player} selected {turn.square.row}, {turn.square.col}
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row},{turn.square.col}
         </li>
       ))}
     </ol>
   );
 }
-
-export default Log;
